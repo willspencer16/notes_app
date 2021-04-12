852f0b05714c01c5da39eb5755083ecf97f46128
jackmcc08 <jackmcc08@gmail.com>
2021-04-12 18:29:34 +0100

    added an array matcher function, and updated the tests in the listSpec to get them to pass, also added some nice images

--- spec/listSpec.js
+++ spec/listSpec.js
@@ -2,14 +2,18 @@
 
 let list = new List();
 let list2 = new List();
-// let Note = "dummyNote";
 let testContent = "This is my first note, I wrote it with Stu.";
+// let testNote = new Note(testContent);
 
-isItTheDuckingSame(list.store.length, 0, 'list.store should be a empty array');
+isItTheDuckingSame(list.store.length, 0, 'list.store should be a empty array with length 0');
+isItTheDuckingSameArray(list.store, [], 'list.store should be a empty array');
 
 list.createNote(testContent)
-isItTheDuckingSame(list.store[0].getContent(), testContent , 'list.store with one note in it should return an array with that one note');
-// isItTheDuckingSame(list.store, ["This is my first note, I wrote it with Stu."] , 'list.store with one note in it should return an array with that one note');
+isItTheDuckingSame(list.store[0].getContent(), testContent , 'list.store with one note in it should return an array with that one note, checking the first item content in array');
+
+// I realised this below test is now kind of pointless as it stores instances of Note, but this means you cannot test equality properly without dummys, so created the below work around which works.
+let testNote = list.store[0]
+isItTheDuckingSameArray(list.store, [testNote] , 'list.store with one note in it should return an array with that one note');
 
 isItTheDuckingSame(list2.createNote(testContent).getContent(), testContent, "Create Note function creates a note");
 
--- testingLib/testFunction.js
+++ testingLib/testFunction.js
@@ -7,3 +7,29 @@ function isItTheDuckingSame(executedFunction, expectedOutput, description) {
     console.log(`%c ${description} - its ducked`,'color: #FF0000')
   }
 }
+
+// The array matcher checks that two arrays are the same length and have the same content in the same order, it requires two arrays to be input - will throw errors if the inputs are not arrays
+function isItTheDuckingSameArray(executedFunction, expectedOutputArray, description) {
+  if (!Array.isArray(executedFunction)) throw 'executed function parameter is not an array';
+  if (!Array.isArray(expectedOutputArray)) throw 'expected output parameter is not an array';
+
+  if (_compareArrays(executedFunction, expectedOutputArray)) {
+    console.log(`%c ${description} - its chilling`,'background: #222; color: #BADA55');
+  } else {
+    console.log(`%c ${description} - its ducked`,'color: #FF0000');
+  }
+
+  function _compareArrays(arr1, arr2) {
+    if (arr1.length !== arr2.length) return false;
+    for (var i = 0; i < arr1.length; i++) {
+  		if (arr1[i] !== arr2[i]) return false;
+  	}
+
+    return true;
+  }
+};
+
+// isItTheDuckingSameArray([1,2], [1,2], "are these the same array")
+// isItTheDuckingSameArray([1,2], [1,2, 3], "are these the same array")
+// // isItTheDuckingSameArray(1, [1,2, 3], "are these the same array")
+// isItTheDuckingSameArray([1,2], 1, "are these the same array")
