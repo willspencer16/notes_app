b0350d50f2b3a5ade27197f8f6b4c0d5509d8c8a
jackmcc08 <jackmcc08@gmail.com>
2021-04-12 17:10:14 +0100

    wrote a bunch of failing test for createNote function as dont have doubles, need to create an array matcher

--- spec/listSpec.js
+++ spec/listSpec.js
@@ -1,5 +1,16 @@
 //isItTheDuckingSame(executedFunction, expectedOutput, description)
 
 let list = new List();
+let list2 = new List();
+// let Note = "dummyNote"
+let testContent = "This is my first note, I wrote it with Stu."
 
 isItTheDuckingSame(list.store.length, 0, 'list.store should be a empty array');
+
+list.createNote(testContent)
+isItTheDuckingSame(list.store[0], testContent , 'list.store with one note in it should return an array with that one note');
+isItTheDuckingSame(list.store, ["This is my first note, I wrote it with Stu."] , 'list.store with one note in it should return an array with that one note');
+
+isItTheDuckingSame(list2.createNote(testContent), testContent, "Create Note function creates a note")
+
+isItTheDuckingSame((list2.createNote(testContent) instanceof Note), true, "Create Note function creates an instance of Note")
--- src/list.js
+++ src/list.js
@@ -2,4 +2,10 @@ class List {
   constructor() {
     this.store = []
   }
-}
+
+  createNote(content) {
+    let newNote = new Note(content)
+    this.store.push(newNote);
+    return newNote
+  }
+ }
